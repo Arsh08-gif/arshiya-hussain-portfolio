8a7590a70e7ae2061068d57880a52ba9ccf93ee2
Arsh08-gif <dkarshiya@outlook.com>
2026-07-08 23:06:47 +0530

    update script to use systemctl instead of tmux

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-echo "Step1: Killing the tmux sessions.."
-
-tmux kill-server 2>/dev/null
-
-echo "Step 2: Entering project folder..."
+echo "Step 1: Entering project folder..."
 
 cd /root/arshiya-hussain-portfolio
 
-echo "Step 3: Pulling latest changes..."
+echo "Step 1: Pulling latest changes..."
 
 git fetch && git reset origin/main --hard
 
-echo "Step 4: Installing dependencies..."
+echo "Step 3: Installing dependencies..."
 
 source /root/arshiya-hussain-portfolio/python3-virtualenv/bin/activate
 
 pip install -r requirements.txt
 
-echo "Step 5: Starting Flask server in tmux..."
+echo "Step 4: Restarting Flask Server..."
+systemctl daemon-reload
+systemctl restart myportfolio
 
-tmux new-session -d -s flask -c /root/arshiya-hussain-portfolio 'source python3-virtualenv/bin/activate && flask run --host=0.0.0.0'
+echo "Deployment Complete!"
+systemctl status myportfolio
